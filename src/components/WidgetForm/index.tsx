280515bc994be7d1566abfcc573db0a7f6e28450
Gustavo Vaz <gustavovaznet@gmail.com>
2022-05-03 11:41:53 -0300

    Create feedbackTypeStep component

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,11 +1,10 @@
-import { CloseButton } from "../CloseButton";
-
 import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG:{
         title: 'Problem',
         image: {
@@ -29,7 +28,7 @@ const feedbackTypes = {
     }
 }
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm(){
 
@@ -37,27 +36,10 @@ export function WidgetForm(){
 
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">Give your feedback</span>
-                <CloseButton />
-            </header>
+            
 
             {!feedbackType ? (
-                <div className="flex py-8 gap-2 w-full">
-                    { Object.entries(feedbackTypes).map(([key, value]) => {
-                        return (
-                            <button
-                                key={key}
-                                onClick={() => setFeedbackType(key as FeedbackType)}
-                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                                type="button"
-                            >
-                                <img src={value.image.source} alt={value.image.alt} />
-                                <span>{value.title}</span>
-                            </button>
-                        )
-                    }) }
-                </div>
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
             ) : (
                 <p>Test</p>
             )}
