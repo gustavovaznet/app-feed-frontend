54070c5203a1172864acd6522152d2853cff2e9e
Gustavo Vaz <gustavovaznet@gmail.com>
2022-05-03 15:02:18 -0300

    Create screenshot function

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
-import { ArrowLeft, Camera } from "phosphor-react";
+import { ArrowLeft } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton"
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps{
     feedbackType: FeedbackType;
@@ -8,6 +10,8 @@ interface FeedbackContentStepProps{
 }
 
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps){
+    
+    const [screenshot, setScreenshot] = useState<string | null>(null)
     const feedbackTypeInfo = feedbackTypes[feedbackType];
     
     return(
@@ -34,9 +38,10 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                 <textarea className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-400 border-zinc-600 bg-transparent rounded-mdfocus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin" placeholder="Tell us more about what's going on..." />
                 <footer className="flex gap-2 mt-2">
                     
-                    <button type="button" className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500">
-                        <Camera className="w-6 h-6"/>
-                    </button>
+                    <ScreenshotButton
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}
+                    />
                     
                     <button type="button" className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors">
                         Send feedback
