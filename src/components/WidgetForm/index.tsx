c8705e2f77350c7000de5db0610524d86a53a3de
Gustavo Vaz <gustavovaznet@gmail.com>
2022-05-03 13:28:12 -0300

    Upload svg assets

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -3,6 +3,7 @@ import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
 
 export const feedbackTypes = {
     BUG:{
@@ -31,8 +32,11 @@ export const feedbackTypes = {
 export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm(){
-
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+    function handleRestartFeedback(){
+        setFeedbackType(null);
+    }
 
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -41,7 +45,10 @@ export function WidgetForm(){
             {!feedbackType ? (
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
             ) : (
-                <p>Test</p>
+                <FeedbackContentStep 
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartFeedback}
+                />
             )}
 
             <footer className="text-xs text-neutral-400">
