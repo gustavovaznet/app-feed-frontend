56d9894f37e10187fbc7cc1be2904a1a697a6277
Gustavo Vaz <gustavovaznet@gmail.com>
2022-05-03 16:02:29 -0300

    Create success file

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton"
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -7,13 +7,27 @@ import { ScreenshotButton } from "../ScreenshotButton";
 interface FeedbackContentStepProps{
     feedbackType: FeedbackType;
     onFeedbackRestartRequested: () => void;
+    onFeedbackSent: () => void;
 }
 
-export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps){
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, onFeedbackSent }: FeedbackContentStepProps){
     
-    const [screenshot, setScreenshot] = useState<string | null>(null)
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+    const [comment, setComment] = useState('');
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
     
+    function handleSubmitFeedback(event: FormEvent){
+        event.preventDefault();
+        console.log({
+            screenshot,
+            comment,
+        })
+
+        onFeedbackSent();
+
+    }
+
     return(
         <>
             <header>
@@ -34,8 +48,12 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                 </span>
                 <CloseButton />                      
             </header>
-            <form className="my-4 w-full">
-                <textarea className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-400 border-zinc-600 bg-transparent rounded-mdfocus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin" placeholder="Tell us more about what's going on..." />
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
+                <textarea
+                    onChange={event => setComment(event.target.value)}
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-400 border-zinc-600 bg-transparent rounded-mdfocus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin" 
+                    placeholder="Tell us more about what's going on..." 
+                />
                 <footer className="flex gap-2 mt-2">
                     
                     <ScreenshotButton
@@ -43,7 +61,11 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                         onScreenshotTook={setScreenshot}
                     />
                     
-                    <button type="button" className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors">
+                    <button
+                        disabled={comment.length === 0}
+                        type="submit" 
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+                    >
                         Send feedback
                     </button>
                 </footer>
--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,7 +1,32 @@
-export function FeedbackSuccessStep(){
+import { CloseButton } from "../../CloseButton";
+import successImageUrl from '../../../assets/success.svg';
+
+interface FeedbackSuccessStepProps{
+    onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep({ onFeedbackRestartRequested }: FeedbackSuccessStepProps){
     return(
-        <div>
-            
-        </div>
+        <>
+            <header>
+                <CloseButton />
+            </header>
+
+            <div className="flex flex-col items-center py-10 w-[384px]">
+                <img className="h-14 w-14" src={successImageUrl} alt="Success image" />
+            </div>
+
+            <span className="text-xl mt-2">
+                Thank you for your feedback!!!
+            </span>
+
+            <button
+                onClick={onFeedbackRestartRequested}
+                type="button"
+                className="py-2 px-6 mt-6 mb-5 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+            >
+                Send another feedback here
+            </button>
+        </>
     )
 }
